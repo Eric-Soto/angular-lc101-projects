447b8847088b0bbc74089a7f7250d80ad0c1595e
EricSoto <ericsoto926@gmail.com>
2020-05-20 21:46:43 -0400

    updated/added images to the fav photos component, updated the styling of movie list and chores

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Pictures';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i7.pngflow.com/pngimage/744/183/png-kappa-internet-troll-meme-forsen-meme-face-photography-monochrome-head-clipart.png';
+  image3 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png';
 
   constructor() { }
 
