a8f748520929aa000143b142130570a80fa72203
EricSoto <ericsoto926@gmail.com>
2020-05-23 18:50:10 -0400

    practice for the NgFor has been worked on

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+   chores = [
+      {title: "Yesterday's Chores", tasks: ['Empty dishwasher', 'Start LaunchCode prep work', 'Buy groceries']},
+      {title: "Today's Chores", tasks: ['Load dishwasher', 'Finish LaunchCode prep work', 'Buy the groceries you forgot']},
+      {title: "Tomorrow's Chores", tasks: ['Empty dishwasher AGAIN', 'Play with LaunchCode practice code', 'Groceries AGAIN']},
+   ]
 
    constructor() { }
 
@@ -15,3 +18,16 @@ export class ChoresComponent implements OnInit {
    }
 
 }
+
+
+// <div class='chores' *ngFor = "let title of todoTitles">
+//     <h3>{{title}}</h3>
+//     <ul>
+//       <li *ngFor = "let chore of chores">{{chore}}</li>
+//     </ul>
+//     <hr>
+//  </div>
+ 
+
+// chores = ['Empty dishwasher', 'Complete LaunchCode prep work'];
+//    todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores", "2036's chores"];
